70a3bab317b69d6cecc68fa29b2fc2f02be638f5
lizhenmingdirk <lizhenmingdirk@126.com>
2023-10-28 11:32:35 +0800

    add neo65 wired for trimode files

--- keyboards/neo/neo65/ansi/config.h
+++ keyboards/neo/neo65/ansi/config.h
@@ -33,8 +33,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #    define RGB_MATRIX_LED_PROCESS_LIMIT  4
 #    define RGB_MATRIX_LED_FLUSH_LIMIT    26
-#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200
-#    define RGB_MATRIX_STARTUP_VAL        128
+// #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 128
+#    define RGB_MATRIX_STARTUP_VAL        64
 #    define DRIVER_ADDR_1                 0b0110000
 #    define DRIVER_COUNT                  1
 #    define RGB_MATRIX_LED_COUNT          68   //DRIVER_LED_TOTAL              //RGB_MATRIX_LED_COUNT 
